4b43faf8f320159a31d4913c649a4048b96d708f
Dylan Moreland <dylan.c.moreland@gmail.com>
2023-03-11 02:13:16 -0800

    Add calculateWinner()

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ function Square({ value, onSquareClick }) {
 }
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(3).fill(null).map(() => new Array(3).fill(null)));
 
   function handleClick(i, j) {
+    if(squares[i][j] || calculateWinner(squares)) return; // don't let user overwrite already filled squares
+
     const nextSquares = squares.slice();
-    nextSquares[i][j] = "X";
+    nextSquares[i][j] = xIsNext ? "X" : "O";
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
@@ -32,4 +36,24 @@ export default function Board() {
       </div>
     </Fragment>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [[0, 0], [0, 1], [0, 2]],
+    [[1, 0], [1, 1], [1, 2]],
+    [[2, 0], [2, 1], [2, 2]],
+    [[0, 0], [1, 0], [2, 0]],
+    [[0, 1], [1, 1], [2, 1]],
+    [[0, 2], [1, 2], [2, 2]],
+    [[0, 0], [1, 1], [2, 2]],
+    [[0, 2], [1, 1], [2, 0]]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [[ai, aj], [bi, bj], [ci, cj]] = lines[i];
+    if (squares[ai][aj] && squares[ai][aj] === squares[bi][bj] && squares[ai][aj] === squares[ci][cj]) {
+      return squares[ai][aj];
+    }
+  }
+  return null;
 }
