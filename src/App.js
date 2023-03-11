4a85389a67446228987b66eeeddad47389684fd5
Dylan Moreland <dylan.c.moreland@gmail.com>
2023-03-11 01:53:28 -0800

    State lifted up to board level

--- src/App.js
+++ src/App.js
@@ -1,32 +1,34 @@
 import React, { Fragment, useState } from "react";
 
-function Square() {
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    setValue("X");
-  }
-
-  return <button className="square" onClick={handleClick}>{value}</button>;
+function Square({ value, onSquareClick }) {
+  return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(3).fill(null).map(() => new Array(3).fill(null)));
+
+  function handleClick(i, j) {
+    const nextSquares = squares.slice();
+    nextSquares[i][j] = "X";
+    setSquares(nextSquares);
+  }
+
   return (
     <Fragment>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0][0]} onSquareClick={() => handleClick(0, 0)} />
+        <Square value={squares[0][1]} onSquareClick={() => handleClick(0, 1)} />
+        <Square value={squares[0][2]} onSquareClick={() => handleClick(0, 2)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[1][0]} onSquareClick={() => handleClick(1, 0)} />
+        <Square value={squares[1][1]} onSquareClick={() => handleClick(1, 1)} />
+        <Square value={squares[1][2]} onSquareClick={() => handleClick(1, 2)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[2][0]} onSquareClick={() => handleClick(2, 0)} />
+        <Square value={squares[2][1]} onSquareClick={() => handleClick(2, 1)} />
+        <Square value={squares[2][2]} onSquareClick={() => handleClick(2, 2)} />
       </div>
     </Fragment>
   );
